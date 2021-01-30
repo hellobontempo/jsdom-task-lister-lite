2f6ab94c71315e82d8be59cd2df7c3bbc50680de
claire <clairebontempo@gmail.com>
2021-01-30 14:25:44 -0800

    save

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
  newTaskForm.addEventListener('submit', handleSubmit)
 
+
  function handleSubmit(event) {
     event.preventDefault();
     const newTask = document.getElementById("new-task-description").value;
-   taskList.innerHTML += `<li class="task">${newTask}</li>`
+   taskList.innerHTML += `<li class="task">${newTask}<button class="button" id=${newTask}>X</button></li>`
    event.target.reset()
-  }
+   document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', event => {
+      taskList.removeChild(event.target.parentElement)
+    })})
 
+ }
+
+
+ 
 });
 
 
